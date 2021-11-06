f4dec0efd6f8a353a03f9f5df5bf7cd4570ce6ed
Kurochkin Mikhail <mkv-1724@mail.ru>
2021-11-07 01:04:35 +0300

    fix C code

--- C/switch.c
+++ C/switch.c
@@ -5,20 +5,20 @@ int main() {
 	int a,i;
 	printf("Enter any digit [0..9]\n");
 	for (i=0;i<=9;i++){
-	scanf("%d",&a);
-	switch (a) {
-		case 0: printf("zero\n"); break;
-		case 1: printf("one\n"); break;
-		case 2: printf("two\n"); break;
-		case 3: printf("three\n"); break;
-		case 4: printf("four\n"); break;
-		case 5: printf("five\n"); break;
-		case 6: printf("seven\n"); break;
-		case 7: printf("six\n"); break;
-		case 8: printf("eight\n"); break;
-		case 9: printf("nine\n"); break;
-    }
-}
+		scanf("%d",&a);
+		switch (a) {
+			case 0: printf("zero\n"); break;
+			case 1: printf("one\n"); break;
+			case 2: printf("two\n"); break;
+			case 3: printf("three\n"); break;
+			case 4: printf("four\n"); break;
+			case 5: printf("five\n"); break;
+			case 6: printf("seven\n"); break;
+			case 7: printf("six\n"); break;
+			case 8: printf("eight\n"); break;
+			case 9: printf("nine\n"); break;
+		}
+	}
  	system("PAUSE");
  	return 0;
 }
